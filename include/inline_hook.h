4c3e469b16ab8fe0784a7f75129ff2136a173a80
qgymib <qgymib@gmail.com>
2021-11-03 23:47:08 +0800

    fix typo

--- include/inline_hook.h
+++ include/inline_hook.h
@@ -6,17 +6,17 @@ extern "C" {
 
 /**
  * @brief Inject function
- * @param[out] origin   Inject Context, also can be called as original function.
- *                      If failure, this value is set to NULL.
- * @param[in] target    The function to be inject
- * @param[in] detour    The function to replace orignal function
- * @return              Inject result
+ * @param[out] origin       Inject Context, also can be called as original function.
+ *                          If failure, this value is set to NULL.
+ * @param[in] target        The function to be inject
+ * @param[in] detour        The function to replace original function
+ * @return                  Inject result
  */
 int inline_hook_inject(void** origin, void* target, void* detour);
 
 /**
  * @brief Uninject function
- * @param[in,out] tump The context to be uninject. This value will be set to NULL.
+ * @param[in,out] origin    The context to be uninject. This value will be set to NULL.
  */
 void inline_hook_uninject(void** origin);
 
